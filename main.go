22d6f8c80cb40971a82aaac668c3d27edb9ab3a1
liyang <Administrator@AFOJE-805021532>
2019-11-29 16:15:05 +0800

    配置文件按环境切换

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"os"
 	_ "poplar/routers"
+	"strings"
 
 	"github.com/astaxie/beego"
 )
+const (
+	envName = "MSF_ENV"
+)
+//配置环境
+func configureEnv(){
+	runMode := os.Getenv(envName)
+	if runMode == ""{
+		runMode = "dev"
+	}else{
+		runMode = strings.ToLower(runMode)
+	}
+	beego.BConfig.RunMode = runMode
+}
 
 func main() {
-	if beego.BConfig.RunMode == "dev" {
-		beego.BConfig.WebConfig.DirectoryIndex = true
-		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
-	}
+	//配置环境
+	configureEnv()
+	//启动服务
 	beego.Run()
 }
