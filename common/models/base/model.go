5ae3739add5731fa41bc83a883bf765ad6f22111
liyang <Administrator@AFOJE-805021532>
2019-12-03 17:57:01 +0800

    优化

--- common/models/base/model.go
+++ common/models/base/model.go
@@ -1,9 +1,10 @@
 package base
+
 import "github.com/astaxie/beego/orm"
 
 type Model struct {
 	TableName string
-	o * orm.Ormer
+	o orm.Ormer
 }
 
 func (m *Model) Table(table string) *Model{
@@ -11,8 +12,18 @@ func (m *Model) Table(table string) *Model{
      return m
 }
 
-func (m *Model) Limit(start interface{},limit interface{}){
+func (m *Model) Limit(start interface{},limit ...interface{}){
+    if len(limit) == 0{
+       // m.o.QueryTable()
+       //m.o.q
+	}
+}
 
+func (m *Model) Init(table string) *Model{
+   return &Model{
+	   TableName: table,
+	   o:         orm.NewOrm(),
+   }
 }
 
 
