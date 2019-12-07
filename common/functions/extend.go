e0abb8fef12f153a5836ccc4a737d6413202f1d5
liyang <654516092@qq.com>
2019-12-07 21:45:42 +0800

    modify extend.go

--- common/functions/extend.go
+++ common/functions/extend.go
@@ -1,2 +1,25 @@
 package functions
 
+import "reflect"
+
+//反射Model、初始化字段名称
+func ReflectModel(structPtr interface{}){
+	rType := reflect.TypeOf(structPtr)
+	rVal := reflect.ValueOf(structPtr)
+	if rType.Kind() == reflect.Ptr {
+		rType = rType.Elem()
+		rVal = rVal.Elem()
+	} else {
+		panic("structPtr must be pointer struct.")
+	}
+	for i := 0; i < rType.NumField(); i++ {
+		t := rType.Field(i)
+		f := rVal.Field(i)
+		key := t.Tag.Get("field")
+		if key == ""{
+			f.Set(reflect.ValueOf(""))
+		}else{
+			f.Set(reflect.ValueOf(key))
+		}
+	}
+}
