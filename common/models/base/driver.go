d37d5e61c7191a84d99bb5cc838c004b23dab663
liyang <654516092@qq.com>
2019-12-07 21:44:00 +0800

    modify driver.go

--- common/models/base/driver.go
+++ common/models/base/driver.go
@@ -5,6 +5,7 @@ package base
 import (
 	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 	"strconv"
@@ -15,11 +16,13 @@ var(
 )
 
 //初始化驱动
-func setDbDriver(){
+func init(){
+	logs.Warning("test init driver.go file")
 	//设置驱动数据库连接参数
 	dataSource := fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=%s",beego.AppConfig.String("db.user"),beego.AppConfig.String("db.pwd"),
 		beego.AppConfig.String("db.host"),beego.AppConfig.String("db.port"),beego.AppConfig.String("db.name"),beego.AppConfig.String("db.charset"))
-	fmt.Println("dataSource:",dataSource)
+	//打印连接数据库参数
+	logs.Warning("databaseDriverConnect:",dataSource)
 	maxIdle,_:= strconv.Atoi(beego.AppConfig.DefaultString("db.maxidle","10"))
 	maxConn,_:= strconv.Atoi(beego.AppConfig.DefaultString("db.maxconn","0"))
 	//设置注册数据库
